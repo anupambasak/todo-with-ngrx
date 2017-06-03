caab6f74d2f75f08a52138c1d29ab4e3b8b9ed9c
Anupam Basak <basak.anupam@cris.org.in>
2017-06-03 22:21:44 +0530

    webpack test

---
+++ src/vendor.ts
@@ -0,0 +1,17 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// ngrx store
+import '@ngrx/store';
+import '@ngrx/store-devtools';
+
+// RxJS
+import 'rxjs/Rx';
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
